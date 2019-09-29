c8dc934db5f1498241204006d79fb0b909180fd2
alessanderleite <alessander.logistica@gmail.com>
2019-09-28 22:28:57 -0300

    implements the interface Imposto

--- src/br/com/alessanderleite/ISS.java
+++ src/br/com/alessanderleite/ISS.java
@@ -1,8 +1,9 @@
 package br.com.alessanderleite;
 
-public class ISS {
-
-	public double calculaISS(Orcamento orcamento) {
+public class ISS implements Imposto {
+	
+	@Override
+	public double calcula(Orcamento orcamento) {
 		return orcamento.getValor() * 0.06;
 	}
 }
