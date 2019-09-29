ebadbafcacddeafbde7c0ff103e279ac07a70fc0
alessanderleite <alessander.logistica@gmail.com>
2019-09-28 22:35:32 -0300

    change method

--- src/br/com/alessanderleite/CalculadorDeImpostos.java
+++ src/br/com/alessanderleite/CalculadorDeImpostos.java
@@ -2,15 +2,9 @@ package br.com.alessanderleite;
 
 public class CalculadorDeImpostos {
 
-	public void realizaCalculo(Orcamento orcamento, String imposto) {
-		
-		if (imposto.equals("ICMS")) {
-			double icms = orcamento.getValor() * 0.1;
-			System.out.println(icms);
-		}
-		else if (imposto.equals("ISS")) {
-			double iss = orcamento.getValor() * 0.06;
-			System.out.println(iss);
-		}
+	public void realizaCalculo(Orcamento orcamento, Imposto impostoQualquer) {
+
+		double imposto = impostoQualquer.calcula(orcamento);
+		System.out.println(imposto);
 	}
 }
