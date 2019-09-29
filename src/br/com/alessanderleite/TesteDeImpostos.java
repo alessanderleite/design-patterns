54d754c8faaa44538e276282903452d7aaadde3e
alessanderleite <alessander.logistica@gmail.com>
2019-09-28 22:37:16 -0300

    create the main class

---
+++ src/br/com/alessanderleite/TesteDeImpostos.java
@@ -0,0 +1,16 @@
+package br.com.alessanderleite;
+
+public class TesteDeImpostos {
+
+	public static void main(String[] args) {
+		Imposto iss = new ISS();
+		Imposto icms = new ICMS();
+		
+		Orcamento orcamento = new Orcamento(500.0);
+		
+		CalculadorDeImpostos calculador = new CalculadorDeImpostos();
+		
+		calculador.realizaCalculo(orcamento, iss);
+		calculador.realizaCalculo(orcamento, icms);
+	}
+}
